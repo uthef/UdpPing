9d86916cea90353303ad9553a6b4bfe8a633f014
Nightcrawler <denbrylski@gmail.com>
2024-06-23 02:22:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+var udpClient = new UdpClient();
+
+while (true)
+{
+    if (udpClient.Available != 0)
+    {
+        var result = await udpClient.ReceiveAsync();
+        await udpClient.SendAsync(result.Buffer, result.RemoteEndPoint);
+    }
+}
